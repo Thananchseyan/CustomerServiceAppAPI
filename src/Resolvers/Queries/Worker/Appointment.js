b0d26286135749983fc974f22990b574567e36b8
Thananchseyan <64280882+Thananchseyan@users.noreply.github.com>
2021-10-24 21:24:24 +0530

    Worker pagination

--- src/Resolvers/Queries/Worker/Appointment.js
+++ src/Resolvers/Queries/Worker/Appointment.js
@@ -1,31 +1,220 @@
 const mongoose=require('mongoose');
 module.exports={
-    worker_getMyAssignedWorks:async (parent,args,{models,user})=>{
+    worker_getMyAssignedWorks:async (parent, {offset,page},{models,user})=>{
         const worker=await models.Worker.findById(user.id);
         if (!worker){
             throw new Error("You have to be a worker");
         }
         try{
-            return models.Appointment.aggregate([
-                {
-                    $unwind:
-                        {
-                            path: "$worker"
+            if (page === 1) {
+                return models.Appointment.aggregate([
+                    {
+                        $unwind:
+                            {
+                                path: "$worker"
+                            }
+                    },
+                    {
+                        $match:
+                            {
+                                worker:mongoose.Types.ObjectId(user.id)
+                            }
+                    },
+                    {
+                        $sort:
+                            {
+                                starting_date: -1
+                            }
+                    },
+                    {
+                        $limit: offset
+                    }
+                ]);
+            } else if (page > 1) {
+                return models.Appointment.aggregate([
+                    {
+                        $unwind:
+                            {
+                                path: "$worker"
+                            }
+                    },
+                    {
+                        $match:
+                            {
+                                worker:mongoose.Types.ObjectId(user.id)
+                            }
+                    },
+                    {
+                        $sort:
+                            {
+                                starting_date: -1
+                            }
+                    },
+                    {
+                        $skip: offset * (page - 1)
+
+                    },
+                    {
+                        $limit: offset
+                    }
+                ]);
+            }
+        }catch (err){
+            console.log(err);
+            throw new Error("Action Failed");
+        }
+    },
+    worker_getMyOngoingWorks: async (parent,{offset,page},{models,user})=>{
+        const worker=await models.Worker.findById(user.id);
+        if (!worker){
+            throw new Error("You cannot query this");
+        }
+        try{
+            if (page === 1) {
+                return models.Appointment.aggregate([
+                    {
+                        $unwind:
+                            {
+                                path: "$worker"
+                            }
+                    },
+                    {
+                        $match:{
+                            $and:
+                            [
+                                {
+                                    worker:mongoose.Types.ObjectId(user.id)
+                                },
+                                {
+                                    state:"going"
+                                }
+                            ]
                         }
-                },
-                {
-                    $match:
-                        {
-                            worker:mongoose.Types.ObjectId(user.id)
+                    },
+                    {
+                        $sort:
+                            {
+                                starting_date: -1
+                            }
+                    },
+                    {
+                        $limit: offset
+                    }
+                ]);
+            } else if (page > 1) {
+                return models.Appointment.aggregate([
+                    {
+                        $unwind:
+                            {
+                                path: "$worker"
+                            }
+                    },
+                    {
+                        $match:{
+                            $and:
+                                [
+                                    {
+                                        worker:mongoose.Types.ObjectId(user.id)
+                                    },
+                                    {
+                                        state:"going"
+                                    }
+                                ]
                         }
-                },
-                {
-                    $sort:
-                        {
-                            starting_date: -1
+                    },
+                    {
+                        $sort:
+                            {
+                                starting_date: -1
+                            }
+                    },
+                    {
+                        $skip: offset * (page - 1)
+
+                    },
+                    {
+                        $limit: offset
+                    }
+                ]);
+            }
+        }catch (err){
+            console.log(err);
+            throw new Error("Action Failed");
+        }
+    },
+    worker_getMyFinishedWorks: async (parent,{offset,page},{models,user})=>{
+        const worker=await models.Worker.findById(user.id);
+        if (!worker){
+            throw new Error("You cannot query this");
+        }
+        try{
+            if (page === 1) {
+                return models.Appointment.aggregate([
+                    {
+                        $unwind:
+                            {
+                                path: "$worker"
+                            }
+                    },
+                    {
+                        $match:{
+                            $and:
+                                [
+                                    {
+                                        worker:mongoose.Types.ObjectId(user.id)
+                                    },
+                                    {
+                                        state:"finished"
+                                    }
+                                ]
                         }
-                }
-            ]);
+                    },
+                    {
+                        $sort:
+                            {
+                                starting_date: -1
+                            }
+                    },
+                    {
+                        $limit: offset
+                    }
+                ]);
+            } else if (page > 1) {
+                return models.Appointment.aggregate([
+                    {
+                        $unwind:
+                            {
+                                path: "$worker"
+                            }
+                    },
+                    {
+                        $match:{
+                            $and:
+                                [
+                                    {
+                                        worker:mongoose.Types.ObjectId(user.id)
+                                    },
+                                    {
+                                        state:"finished"
+                                    }
+                                ]
+                        }
+                    },
+                    {
+                        $sort:
+                            {
+                                starting_date: -1
+                            }
+                    },
+                    {
+                        $skip: offset * (page - 1)
+
+                    },
+                    {
+                        $limit: offset
+                    }
+                ]);
+            }
         }catch (err){
             console.log(err);
             throw new Error("Action Failed");
--- src/Resolvers/Queries/Worker/NotificationQueries.js
+++ src/Resolvers/Queries/Worker/NotificationQueries.js
@@ -1,32 +1,63 @@
 const mongoose=require('mongoose');
 module.exports = {
-    worker_getMyNotification:async (parent,args,{models,user})=>{
+    worker_getMyNotification:async (parent, {offset,page},{models,user})=> {
         console.log(user.id);
         console.log(user._id);
-        const worker=await models.Worker.findById(user.id);
-        if (!worker){
+        const worker = await models.Worker.findById(user.id);
+        if (!worker) {
             throw new Error("You cannot query this");
         }
-        try{
-            return models.NotificationWorker.aggregate([
-                {
-                    $match:
-                        {
-                            worker:mongoose.Types.ObjectId(user.id)
-                        }
+        try {
+            if (page === 1) {
+                return models.NotificationWorker.aggregate([
+                    {
+                        $match:
+                            {
+                                worker: mongoose.Types.ObjectId(user.id)
+                            }
 
-                },
-                {
-                    $sort:
-                        {
-                            date:-1
-                        }
+                    },
+                    {
+                        $sort:
+                            {
+                                date: -1
+                            }
 
-                }
-            ]);
-        }catch (err){
-            console.log(err);
-            throw new Error("Error in fetching notification");
+                    },
+                    {
+                        $limit: offset
+                    }
+                ]);
+            } else if (page > 1) {
+                return models.NotificationWorker.aggregate([
+                    {
+                        $match:
+                            {
+                                worker: mongoose.Types.ObjectId(user.id)
+                            }
+
+                    },
+                    {
+                        $sort:
+                            {
+                                date: -1
+                            }
+
+                    },
+                    {
+                        $skip: offset * (page - 1)
+
+                    },
+                    {
+                        $limit: offset
+                    }
+                ]);
+            }
+        }catch
+            (err)
+            {
+                console.log(err);
+                throw new Error("Error in fetching notification");
+            }
         }
-    }
 }
