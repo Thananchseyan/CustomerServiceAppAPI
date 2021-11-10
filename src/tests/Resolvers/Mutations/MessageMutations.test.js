2199ab0011a95f54877a6c687a2493799bf19795
Thananchseyan <64280882+Thananchseyan@users.noreply.github.com>
2021-11-10 18:08:15 +0530

    message mutations test added

---
+++ src/tests/Resolvers/Mutations/MessageMutations.test.js
@@ -0,0 +1,53 @@
+const messageMutationsTest=require('../../../Resolvers/Mutations/MessageMutations');
+describe("given mutations are about messages",()=>{
+    describe("when send message reqd",()=>{
+        it("should create in Message Models",function (){
+            const mock=jest.fn()
+            const models={
+                ServiceProvider:{
+                    findById:mock
+                },
+                Moderator:{
+                    findById:mock
+                },
+                Customer:{
+                    findById:mock
+                },
+                Worker:{
+                    findById:mock
+                },
+                Messsage:{
+                    create:mock
+                }
+            }
+            const data={}
+            const user={}
+            expect(messageMutationsTest.sendMessage({},data,{models,user})).rejects.toThrow("You have to signin to send message");
+        })
+    })
+    describe("when read message req",()=>{
+        it("should update in Message Models",function (){
+            const mock=jest.fn()
+            const models={
+                ServiceProvider:{
+                    findById:mock
+                },
+                Moderator:{
+                    findById:mock
+                },
+                Customer:{
+                    findById:mock
+                },
+                Worker:{
+                    findById:mock
+                },
+                Messsage:{
+                    updateOne:mock
+                }
+            }
+            const data={}
+            const user={}
+            expect(messageMutationsTest.readMessage({},data,{models,user})).rejects.toThrow("You have to signin to send message");
+        })
+    })
+})
